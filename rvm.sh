3db31484995b71ce19ac135fff376e90d1e7e181
Mike Carifio <carif@pazien.com>
2014-11-16 21:55:34 -0500

    add --ruby

--- rvm.sh
+++ rvm.sh
@@ -2,5 +2,5 @@
 
 # http://rvm.io/rvm/install
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-\curl -sSL https://get.rvm.io  | bash -s stable --rails
+\curl -sSL https://get.rvm.io  | bash -s stable --ruby --rails
 
